689872fb98f4cad17fb763ad1af05a2858f94e79
Gooseman <theados123@gmail.com>
2023-01-13 17:21:46 +0100

    Set up basic data fetching feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import { useState } from "react";
 import { css } from "@emotion/react";
+import GetProductPage from "./queries/GetProductPage";
 
 function App() {
+  const [clicked, setClicked] = useState(false);
+
+  function handleClick() {
+    setClicked(true);
+  }
+
   return (
     <div className="App">
       <h1
@@ -9,9 +16,11 @@ function App() {
           color: red;
         `}
       >
-        Product list
+        The product list
       </h1>
-      <h5>Your number one source of products</h5>
+      <h5>Your #1 source of products</h5>
+      <button onClick={() => handleClick()}>Fetch stuff</button>
+      {clicked && <GetProductPage pageNumber={1} />}
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,18 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </Provider>
   </React.StrictMode>
 );

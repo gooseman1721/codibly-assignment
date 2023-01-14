8c65fef544921ae3b3d3712914bbe470bfef77be
Gooseman <theados123@gmail.com>
2023-01-15 00:02:04 +0100

    Removed unused Redux slice.

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,12 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/dist/query";
-import { getDefaultState } from "react-query/types/core/mutation";
-import productReducer from "../features/product/productSlice";
 import { productApi } from "../services/productAPI";
 
 export const store = configureStore({
   reducer: {
-    product: productReducer,
     [productApi.reducerPath]: productApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>

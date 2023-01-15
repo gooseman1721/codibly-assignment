0f7b6b682a4ddbb1abe5eb0244416101c997510e
Gooseman <theados123@gmail.com>
2023-01-15 01:17:54 +0100

    Removed React-Query components. Added new path for product id

--- src/main.tsx
+++ src/main.tsx
@@ -3,19 +3,19 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
-import { QueryClient, QueryClientProvider } from "react-query";
-import { ReactQueryDevtools } from "react-query/devtools";
 import {
   createBrowserRouter,
   RouterProvider,
   redirect,
 } from "react-router-dom";
 
-const queryClient = new QueryClient();
-
 const router = createBrowserRouter([
   {
     path: "page/:pageNumber",
+    element: <App />,
+  },
+  {
+    path: "product/:productId",
     element: <App />,
   },
   {
@@ -30,10 +30,7 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
-        <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
 );

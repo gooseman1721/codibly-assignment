afe60eaa438863b394359dacd4a960d75d4c6ab5
Gooseman <theados123@gmail.com>
2023-01-16 18:12:00 +0100

    Added a hover animation to Product component

--- src/Product.tsx
+++ src/Product.tsx
@@ -9,8 +9,27 @@ const containerStyle = css`
   justify-content: space-between;
   padding: 1rem;
   margin-bottom: 20px;
+  border: 3px black solid;
+  border-radius: 10px;
 
-  box-shadow: 7px 7px 0px black;
+  box-shadow: 4px 4px 0px black;
+
+  /* @keyframes slide-left {
+    from {
+      transform: translateX(500%);
+    }
+    to {
+      transform: translateX(0%);
+    }
+  }
+  animation: 0.5s slide-left; */
+
+  transition: all 0.05s ease-in-out;
+  &:hover {
+    cursor: pointer;
+    transition: all 0.05s ease-in-out;
+    transform: scale(105%);
+  }
 `;
 
 

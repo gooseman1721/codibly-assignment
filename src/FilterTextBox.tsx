c23de23733e42e99f2bb7a0c5d1985ab8c313828
Gooseman <theados123@gmail.com>
2023-01-16 19:24:45 +0100

    Added input filtering to FilterTextBox, only numbers can be entered.

--- src/FilterTextBox.tsx
+++ src/FilterTextBox.tsx
@@ -5,13 +5,32 @@ export default function FilterTextBox(props: { itemTotal: number }) {
   const { itemTotal } = props;
   const [inputValue, setInputValue] = useState("1");
 
+  function filterChars(textBoxValue: string) {
+    if (/[0-9]/.test(textBoxValue.slice(-1)) || textBoxValue === "") {
+      setInputValue(textBoxValue);
+    }
+  }
+
+  function filterCharsOnPaste(textBoxValue: string) {
+    if (/[0-9]/.test(textBoxValue)) {
+      setInputValue(textBoxValue);
+    }
+  }
+
   return (
     <div>
       <input
-        type={"number"}
+        type={"text"}
         name={"Filter product ID"}
+        placeholder={"Enter product ID"}
         value={inputValue}
-        onChange={(e) => setInputValue(e.target.value)}
+        onChange={(e) => {
+          filterChars(e.target.value);
+        }}
+        onPaste={(e) => {
+          e.preventDefault();
+          filterCharsOnPaste(e.clipboardData.getData("text"));
+        }}
         min={1}
         max={itemTotal}
       />
